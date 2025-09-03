6c7fcdd7cd7e13d908a9a27258d359de47269344
csning1998 <january20070120@gmail.com>
2025-09-03 16:35:59 +0800

    refactor(workstation-provider): Remove all workstation-related files

--- terraform/modules/provisioner-workstation/output.tf
+++
@@ -1,9 +0,0 @@
-output "all_nodes" {
-  description = "List of all nodes (master and workers) in the standard format."
-  value       = local.all_nodes
-}
-
-output "vm_status" {
-  description = "The status ID of the VM readiness check resource."
-  value       = { for key, res in null_resource.configure_nodes : key => res.id }
-}
