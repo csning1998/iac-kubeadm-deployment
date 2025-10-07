3d5c046c8eb1de833da65782bf4eb701728a28de
csning1998 <january20070120@gmail.com>
2025-10-07 21:40:20 +0800

    fix(vm-networking): Align NAT gateway IP between Libvirt and cloud-init. For instance, it was libvirt 192.168.68.1 vs cloud-init 192.168.68.2

--- terraform/layers/30-registry-provision/locals.tf
+++ terraform/layers/30-registry-provision/locals.tf
@@ -6,7 +6,7 @@ locals {
   }
   ansible_root_path = abspath("${path.root}/../../../ansible")
 
-  registry_nat_network_gateway       = cidrhost(var.registry_infrastructure.network.nat.cidr, 2)
+  registry_nat_network_gateway       = cidrhost(var.registry_infrastructure.network.nat.cidr, 1)
   registry_nat_network_subnet_prefix = join(".", slice(split(".", split("/", var.registry_infrastructure.network.nat.cidr)[0]), 0, 3))
   ssh_content_registry = flatten([
     for key, node in local.all_nodes_map : {
