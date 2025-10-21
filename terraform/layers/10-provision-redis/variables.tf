f24b897c569dfd6c3b5b106e779a8c608f09e2ec
csning1998 <january20070120@gmail.com>
2025-10-21 17:29:26 +0800

    feat(terraform): Add `10-provision-redis` and `15-bootstrapper-ansible-redis` modules to drive Playbook

---
+++ terraform/layers/10-provision-redis/variables.tf
@@ -0,0 +1,39 @@
+
+# Registry Server Topology & Configuration
+
+variable "redis_cluster_config" {
+  description = "Define the registry server including virtual hardware resources."
+  type = object({
+    cluster_name = string
+    nodes = object({
+      redis = list(object({
+        ip   = string
+        vcpu = number
+        ram  = number
+      }))
+    })
+    base_image_path = optional(string, "../../../packer/output/05-base-redis/ubuntu-server-24-05-base-redis.qcow2")
+  })
+}
+
+# Registry Server Infrastructure Network Configuration
+
+variable "redis_infrastructure" {
+  description = "All Libvirt-level infrastructure configurations for the Redis Service."
+  type = object({
+    network = object({
+      nat = object({
+        name        = string
+        cidr        = string
+        bridge_name = string
+      })
+      hostonly = object({
+        name        = string
+        cidr        = string
+        bridge_name = string
+      })
+    })
+    redis_allowed_subnet = optional(string, "172.16.137.0/24")
+    storage_pool_name    = optional(string, "iac-redis")
+  })
+}
